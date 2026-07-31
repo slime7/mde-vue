5b86080f6b375923d15dc6691370ad7ee0b2bf95
slime7 <slime7@foxmail.com>
2026-07-31 12:15:18 +0800

    fix(build): 补充 ESM 默认解析条件

--- tests/exports.spec.js
+++ tests/exports.spec.js
@@ -236,6 +236,7 @@ describe('公共组件导出', () => {
       .filter((file) => file.endsWith('.js'));
 
     expect(Object.keys(packageJson.exports)).toEqual(['.', './styles.css']);
+    expect(packageJson.exports['.'].default).toBe(packageJson.exports['.'].import);
     expect(readdirSync('dist').sort()).toEqual(['index.d.ts', 'mdu-ui.js', 'styles.css']);
     expect(javascriptFiles).toEqual(['mdu-ui.js']);
     expect(readFileSync('dist/mdu-ui.js', 'utf8')).not.toMatch(/\.vue(?:['"]|\?)/);
