7e72bdc59b8ad42d8f5e2ccf0a4ddbaa9c940e15
slime7 <slime7@foxmail.com>
2026-08-17 08:54:03 +0800

    docs(button): 默认按钮示例补充 prefix 与 toggle 状态展示

--- docs/site/examples/button/ButtonDefaultOnlyExample.vue
+++ docs/site/examples/button/ButtonDefaultOnlyExample.vue
@@ -1,5 +1,30 @@
 <!-- #region template -->
 <template>
-  <mat-btn>确认</mat-btn>
+  <div class="flex grow">
+    <div class="flex-1 flex flex-col gap-y-2">
+      <div class="text-center">
+        <mat-btn prefix="edit">
+          label
+        </mat-btn>
+      </div>
+      <div class="text-center">
+        default button
+      </div>
+    </div>
+
+    <div class="flex-1 flex flex-col gap-y-2">
+      <div class="flex gap-x-4 justify-center">
+        <mat-btn toggle prefix="edit">
+          unselected
+        </mat-btn>
+        <mat-btn toggle selected prefix="edit">
+          selected
+        </mat-btn>
+      </div>
+      <div class="text-center">
+        toggle button
+      </div>
+    </div>
+  </div>
 </template>
 <!-- #endregion template -->
